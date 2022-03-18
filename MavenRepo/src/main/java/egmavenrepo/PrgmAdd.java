58154e172a2d2070e9afad8bc8646c9004b0ad6a
arshapresannan <101731042+arshapresannan@users.noreply.github.com>
2022-03-18 08:26:02 +0530

    Uploaded successfully

---
+++ MavenRepo/src/main/java/egmavenrepo/PrgmAdd.java
@@ -0,0 +1,13 @@
+package egmavenrepo;
+
+public class PrgmAdd {
+
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=6;
+				int c=a+b;
+				System.out.println("Result is: "+c);
+	}
+
+}
---
+++ MavenRepo/src/main/java/egmavenrepo/PrgmSub.java
@@ -0,0 +1,16 @@
+package egmavenrepo;
+
+public class PrgmSub {
+
+	public static void main(String[] args) {
+		
+			
+			int a=10;
+			int b=6;
+					int c=a-b;
+					System.out.println("Result is: "+c);
+		}
+
+	}
+
+
